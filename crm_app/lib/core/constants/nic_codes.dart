c6be4c5252d3aeae4f0c1d7d76d50637371d2b99
Yovel-R <yovel2911@gmail.com>
2026-06-11 17:04:37 +0530

    nic

--- crm_app/lib/core/constants/nic_codes.dart
+++ crm_app/lib/core/constants/nic_codes.dart
@@ -1,63 +1,88 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 
 class NicCode {
   final String code;
   final String description;
   final String type;
+  final String section;
+  final String sectionTitle;
 
   const NicCode({
     required this.code,
     required this.description,
     required this.type,
+    this.section = '',
+    this.sectionTitle = '',
   });
 }
 
-const List<NicCode> allNicCodes = [
-  NicCode(code: '6201', description: 'Computer programming activities', type: 'Class'),
-  NicCode(code: '62011', description: 'Writing, modifying, testing of computer program to meet the needs of a particular client excluding web-page designing', type: 'Sub-class'),
-  NicCode(code: '62012', description: 'Web-page designing', type: 'Sub-class'),
-  NicCode(code: '62013', description: 'Providing software support and maintenance to the clients', type: 'Sub-class'),
-  NicCode(code: '6202', description: 'Computer consultancy and computer facilities management activities', type: 'Class'),
-  NicCode(code: '62020', description: 'Computer consultancy and computer facilities management activities', type: 'Sub-class'),
-  NicCode(code: '6209', description: 'Other information technology and computer service activities', type: 'Class'),
-  NicCode(code: '62091', description: 'Software installation', type: 'Sub-class'),
-  NicCode(code: '62092', description: 'Computer disaster recovery', type: 'Sub-class'),
-  NicCode(code: '62099', description: 'Other information technology and computer service activities n.e.c', type: 'Sub-class'),
-  NicCode(code: '6311', description: 'Data processing, hosting and related activities', type: 'Class'),
-  NicCode(code: '63111', description: 'Data processing activities', type: 'Sub-class'),
-  NicCode(code: '63112', description: 'Web hosting activities', type: 'Sub-class'),
-  NicCode(code: '6312', description: 'Web portals', type: 'Class'),
-  NicCode(code: '63121', description: 'Operation of web sites that use a search engine to generate and maintain extensive databases', type: 'Sub-class'),
-  NicCode(code: '63122', description: 'Operation of other websites that act as portals to the Internet', type: 'Sub-class'),
-  NicCode(code: '6920', description: 'Accounting, bookkeeping and auditing activities; tax consultancy', type: 'Class'),
-  NicCode(code: '69201', description: 'Accounting, bookkeeping and auditing activities', type: 'Sub-class'),
-  NicCode(code: '69202', description: 'Tax consultancy', type: 'Sub-class'),
-  NicCode(code: '7020', description: 'Management consultancy activities', type: 'Class'),
-  NicCode(code: '70200', description: 'Management consultancy activities', type: 'Sub-class'),
-  NicCode(code: '7310', description: 'Advertising', type: 'Class'),
-  NicCode(code: '73100', description: 'Advertising', type: 'Sub-class'),
-  NicCode(code: '4711', description: 'Retail sale in non-specialized stores with food, beverages or tobacco predominating', type: 'Class'),
-  NicCode(code: '47110', description: 'Retail sale in non-specialized stores with food, beverages or tobacco predominating', type: 'Sub-class'),
-  NicCode(code: '4791', description: 'Retail sale via mail order houses or via Internet', type: 'Class'),
-  NicCode(code: '47911', description: 'Retail sale via mail order houses', type: 'Sub-class'),
-  NicCode(code: '47912', description: 'Retail sale via Internet', type: 'Sub-class'),
-  NicCode(code: '5610', description: 'Restaurants and mobile food service activities', type: 'Class'),
-  NicCode(code: '56101', description: 'Restaurants without bars', type: 'Sub-class'),
-  NicCode(code: '56102', description: 'Cafeterias, fast-food restaurants and other food preparation in market stalls', type: 'Sub-class'),
-  NicCode(code: '5621', description: 'Event catering', type: 'Class'),
-  NicCode(code: '56210', description: 'Event catering', type: 'Sub-class'),
-  NicCode(code: '8510', description: 'Primary education', type: 'Class'),
-  NicCode(code: '85101', description: 'Pre-primary education (kindergarten and nurseries)', type: 'Sub-class'),
-  NicCode(code: '85102', description: 'Primary education', type: 'Sub-class'),
-  NicCode(code: '8521', description: 'General secondary education', type: 'Class'),
-  NicCode(code: '85211', description: 'General school education in the first stage of the secondary level', type: 'Sub-class'),
-  NicCode(code: '85212', description: 'General school education in the second stage of the secondary level (Senior secondary)', type: 'Sub-class'),
-  NicCode(code: '8620', description: 'Medical and dental practice activities', type: 'Class'),
-  NicCode(code: '86201', description: 'Medical practice activities', type: 'Sub-class'),
-  NicCode(code: '86202', description: 'Dental practice activities', type: 'Sub-class'),
-  NicCode(code: '9602', description: 'Hairdressing and other beauty treatment', type: 'Class'),
-  NicCode(code: '96020', description: 'Hairdressing and other beauty treatment', type: 'Sub-class'),
-  NicCode(code: '9609', description: 'Other personal service activities n.e.c.', type: 'Class'),
-  NicCode(code: '96098', description: 'Pet care services', type: 'Sub-class'),
-  // We can add thousands more here using a JSON loader in the future.
-];
+class NicCodeService {
+  static Future<List<NicCode>> loadNicCodes() async {
+    try {
+      final String response = await rootBundle.loadString('assets/json/NIC_2008_classification.json');
+      final data = await json.decode(response);
+      
+      List<NicCode> codes = [];
+      final sections = data['NIC_2008']?['sections'] ?? [];
+      
+      for (var sec in sections) {
+        final sectionCode = sec['section'] ?? '';
+        final sectionTitle = sec['title'] ?? '';
+        
+        for (var div in sec['divisions'] ?? []) {
+          if (div['division'] != null) {
+            codes.add(NicCode(
+              code: div['division'],
+              description: div['title'] ?? '',
+              type: 'Division',
+              section: sectionCode,
+              sectionTitle: sectionTitle,
+            ));
+          }
+          
+          for (var grp in div['groups'] ?? []) {
+            if (grp['code'] != null) {
+              codes.add(NicCode(
+                code: grp['code'],
+                description: grp['description'] ?? '',
+                type: 'Group',
+                section: sectionCode,
+                sectionTitle: sectionTitle,
+              ));
+            }
+            
+            for (var cls in grp['classes'] ?? []) {
+              if (cls['code'] != null) {
+                codes.add(NicCode(
+                  code: cls['code'],
+                  description: cls['description'] ?? '',
+                  type: 'Class',
+                  section: sectionCode,
+                  sectionTitle: sectionTitle,
+                ));
+              }
+              
+              for (var sub in cls['sub_classes'] ?? []) {
+                if (sub['code'] != null) {
+                  codes.add(NicCode(
+                    code: sub['code'],
+                    description: sub['description'] ?? '',
+                    type: 'Sub-class',
+                    section: sectionCode,
+                    sectionTitle: sectionTitle,
+                  ));
+                }
+              }
+            }
+          }
+        }
+      }
+      return codes;
+    } catch (e) {
+      debugPrint('Error loading NIC codes: $e');
+      return [];
+    }
+  }
+}
--- webpage/src/app/client/tools/nic-finder/nic-finder.ts
+++ webpage/src/app/client/tools/nic-finder/nic-finder.ts
@@ -1,11 +1,14 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, signal, computed, OnInit, HostListener, ElementRef } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 interface NicCode {
   code: string;
   description: string;
   type: string;
+  section: string;
+  sectionTitle: string;
 }
 
 @Component({
@@ -18,67 +21,92 @@ interface NicCode {
 export class NicFinder {
   searchQuery = signal('');
 
-  allNicCodes: NicCode[] = [
-    {code: '6201', description: 'Computer programming activities', type: 'Class'},
-    {code: '62011', description: 'Writing, modifying, testing of computer program to meet the needs of a particular client excluding web-page designing', type: 'Sub-class'},
-    {code: '62012', description: 'Web-page designing', type: 'Sub-class'},
-    {code: '62013', description: 'Providing software support and maintenance to the clients', type: 'Sub-class'},
-    {code: '6202', description: 'Computer consultancy and computer facilities management activities', type: 'Class'},
-    {code: '62020', description: 'Computer consultancy and computer facilities management activities', type: 'Sub-class'},
-    {code: '6209', description: 'Other information technology and computer service activities', type: 'Class'},
-    {code: '62091', description: 'Software installation', type: 'Sub-class'},
-    {code: '62092', description: 'Computer disaster recovery', type: 'Sub-class'},
-    {code: '62099', description: 'Other information technology and computer service activities n.e.c', type: 'Sub-class'},
-    {code: '6311', description: 'Data processing, hosting and related activities', type: 'Class'},
-    {code: '63111', description: 'Data processing activities', type: 'Sub-class'},
-    {code: '63112', description: 'Web hosting activities', type: 'Sub-class'},
-    {code: '6312', description: 'Web portals', type: 'Class'},
-    {code: '63121', description: 'Operation of web sites that use a search engine to generate and maintain extensive databases', type: 'Sub-class'},
-    {code: '63122', description: 'Operation of other websites that act as portals to the Internet', type: 'Sub-class'},
-    {code: '6920', description: 'Accounting, bookkeeping and auditing activities; tax consultancy', type: 'Class'},
-    {code: '69201', description: 'Accounting, bookkeeping and auditing activities', type: 'Sub-class'},
-    {code: '69202', description: 'Tax consultancy', type: 'Sub-class'},
-    {code: '7020', description: 'Management consultancy activities', type: 'Class'},
-    {code: '70200', description: 'Management consultancy activities', type: 'Sub-class'},
-    {code: '7310', description: 'Advertising', type: 'Class'},
-    {code: '73100', description: 'Advertising', type: 'Sub-class'},
-    {code: '4711', description: 'Retail sale in non-specialized stores with food, beverages or tobacco predominating', type: 'Class'},
-    {code: '47110', description: 'Retail sale in non-specialized stores with food, beverages or tobacco predominating', type: 'Sub-class'},
-    {code: '4791', description: 'Retail sale via mail order houses or via Internet', type: 'Class'},
-    {code: '47911', description: 'Retail sale via mail order houses', type: 'Sub-class'},
-    {code: '47912', description: 'Retail sale via Internet', type: 'Sub-class'},
-    {code: '5610', description: 'Restaurants and mobile food service activities', type: 'Class'},
-    {code: '56101', description: 'Restaurants without bars', type: 'Sub-class'},
-    {code: '56102', description: 'Cafeterias, fast-food restaurants and other food preparation in market stalls', type: 'Sub-class'},
-    {code: '5621', description: 'Event catering', type: 'Class'},
-    {code: '56210', description: 'Event catering', type: 'Sub-class'},
-    {code: '8510', description: 'Primary education', type: 'Class'},
-    {code: '85101', description: 'Pre-primary education (kindergarten and nurseries)', type: 'Sub-class'},
-    {code: '85102', description: 'Primary education', type: 'Sub-class'},
-    {code: '8521', description: 'General secondary education', type: 'Class'},
-    {code: '85211', description: 'General school education in the first stage of the secondary level', type: 'Sub-class'},
-    {code: '85212', description: 'General school education in the second stage of the secondary level (Senior secondary)', type: 'Sub-class'},
-    {code: '8620', description: 'Medical and dental practice activities', type: 'Class'},
-    {code: '86201', description: 'Medical practice activities', type: 'Sub-class'},
-    {code: '86202', description: 'Dental practice activities', type: 'Sub-class'},
-    {code: '9602', description: 'Hairdressing and other beauty treatment', type: 'Class'},
-    {code: '96020', description: 'Hairdressing and other beauty treatment', type: 'Sub-class'},
-    {code: '9609', description: 'Other personal service activities n.e.c.', type: 'Class'},
-    {code: '96098', description: 'Pet care services', type: 'Sub-class'}
-  ];
+  allNicCodes = signal<NicCode[]>([]);
 
   filteredCodes = computed(() => {
     const query = this.searchQuery().toLowerCase().trim();
+    const codes = this.allNicCodes();
     if (!query) {
-      return this.allNicCodes;
+      return codes;
     }
-    return this.allNicCodes.filter(c => 
+    return codes.filter(c => 
       c.code.toLowerCase().includes(query) || 
       c.description.toLowerCase().includes(query)
     );
   });
 
-  constructor(public location: Location) {}
+  showScrollTop = signal(false);
+
+  constructor(public location: Location, private http: HttpClient, private el: ElementRef) {}
+
+  @HostListener('scroll')
+  onScroll() {
+    const scrollTop = this.el.nativeElement.scrollTop;
+    this.showScrollTop.set(scrollTop > 300);
+  }
+
+  scrollToTop() {
+    this.el.nativeElement.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  ngOnInit() {
+    this.http.get<any>('assets/json/NIC_2008_classification.json').subscribe({
+      next: (data) => {
+        const codes: NicCode[] = [];
+        const sections = data?.NIC_2008?.sections || [];
+        for (const sec of sections) {
+          for (const div of sec.divisions || []) {
+            if (div.division) {
+              codes.push({
+                code: div.division,
+                description: div.title,
+                type: 'Division',
+                section: sec.section || '',
+                sectionTitle: sec.title || ''
+              });
+            }
+            for (const grp of div.groups || []) {
+              if (grp.code) {
+                codes.push({
+                  code: grp.code,
+                  description: grp.description,
+                  type: 'Group',
+                  section: sec.section || '',
+                  sectionTitle: sec.title || ''
+                });
+              }
+              for (const cls of grp.classes || []) {
+                if (cls.code) {
+                  codes.push({
+                    code: cls.code,
+                    description: cls.description,
+                    type: 'Class',
+                    section: sec.section || '',
+                    sectionTitle: sec.title || ''
+                  });
+                }
+                for (const sub of cls.sub_classes || []) {
+                  if (sub.code) {
+                    codes.push({
+                      code: sub.code,
+                      description: sub.description,
+                      type: 'Sub-class',
+                      section: sec.section || '',
+                      sectionTitle: sec.title || ''
+                    });
+                  }
+                }
+              }
+            }
+          }
+        }
+        this.allNicCodes.set(codes);
+      },
+      error: (err) => console.error('Failed to load NIC codes:', err)
+    });
+  }
+
+
 
   goBack() {
     this.location.back();
