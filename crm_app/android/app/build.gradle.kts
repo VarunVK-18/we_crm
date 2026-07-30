e96839c1a9b16e51ddefabdc969f129f64e5873c
Yovel-R <yovel2911@gmail.com>
2026-07-30 16:22:02 +0530

    pakage update

--- crm_app/android/app/build.gradle.kts
+++ crm_app/android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 android {
-    namespace = "com.softrate.wecrm"
+    namespace = "com.wealthempires.startupdoctor"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -41,7 +41,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.softrate.wecrm"
+        applicationId = "com.wealthempires.startupdoctor"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- crm_app/android/app/src/main/kotlin/com/wealthempires/startupdoctor/MainActivity.kt
+++ crm_app/android/app/src/main/kotlin/com/wealthempires/startupdoctor/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.softrate.wecrm
+package com.wealthempires.startupdoctor
 
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
